07d7e3f61dd929d8164bd6dd127d94f23ce46160
PRAVEEN KUSHWAHA <praveenkushwaha.tech@gmail.com>
2025-05-25 20:30:55 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import EmployeeComponent from './component‎/EmployeeComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <EmployeeComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
